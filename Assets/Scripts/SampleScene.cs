2bf15239cad904a625831b2ad0c1b2caf338408d
nakano yosuke <naninuneno0411@gmail.com>
2019-02-27 00:40:17 +0900

    add scene script

---
+++ Assets/Scripts/SampleScene.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class SampleScene : MonoBehaviour {
+    [SerializeField] Text printText = default;
+    void Start() {
+        printText.text = "Any Message Here.";
+    }
+
+    void Update() {
+
+    }
+}
