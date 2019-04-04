e1da5fa9c127908a04a38295c8ca95c404cab4a5
nakano yosuke <naninuneno0411@gmail.com>
2019-04-05 00:22:28 +0900

    move demo scripts

---
+++ Assets/GrayBlueDemo/Scripts/MonoPeripheralExample.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using GrayBlue;
+
+namespace GrayBlueDemo {
+    public class MonoPeripheralExample : MonoPeripheralBase {
+        Quaternion baseRotarion = Quaternion.identity;
+
+        void OnDisable() {
+            Peripheral?.UnlistenEvent();
+        }
+
+        void OnDestroy() {
+            Peripheral?.Dispose();
+        }
+
+        protected override void OnPeripheralLost() {
+            Destroy(gameObject);
+        }
+
+        protected override void OnIMUSensorUpdate(IMUData imu) {
+            var q = imu.unity.quat;
+            if (baseRotarion == Quaternion.identity) {
+                baseRotarion = q;
+            }
+            transform.rotation = Quaternion.Inverse(baseRotarion) * q;
+        }
+
+        protected override void OnButtonPush(string button) {
+            // base pose reset
+            baseRotarion = Quaternion.identity;
+        }
+    }
+}
