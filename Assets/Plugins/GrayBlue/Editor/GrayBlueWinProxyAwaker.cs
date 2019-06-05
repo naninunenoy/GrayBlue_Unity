66d9544aa2846f340da3abb91a8857c4734c68ba
nakano yosuke <naninuneno0411@gmail.com>
2019-06-06 01:33:17 +0900

    add all of GrayBlue_WinProxy components to project root

--- Assets/Plugins/GrayBlue/Editor/GrayBlueWinProxyAwaker.cs
+++ Assets/Plugins/GrayBlue/Editor/GrayBlueWinProxyAwaker.cs
@@ -7,7 +7,7 @@ namespace GrayBlue.Editor {
 
     [InitializeOnLoadAttribute]
     public class GrayBlueWinProxyAwaker {
-        const string exePath = "Plugins/GrayBlue/Editor/GrayBlue_WinProxy.exe";
+        const string exePath = "ExternalTools/GrayBlue_WinProxy/GrayBlue_WinProxy.exe";
         static Process grayBlueExe = default;
         static GrayBlueWinProxyAwaker() {
             EditorApplication.playModeStateChanged += EditorApplication_playModeStateChanged;
@@ -17,15 +17,21 @@ namespace GrayBlue.Editor {
             if (state == PlayModeStateChange.EnteredPlayMode) {
                 // GrayBlue_WinProxyを起動
                 try {
+                    var projectRootPath= System.IO.Directory.GetParent(Application.dataPath).FullName;
                     grayBlueExe = new Process();
-                    grayBlueExe.StartInfo.FileName = $"{Application.dataPath}/{exePath}";
+                    grayBlueExe.StartInfo.FileName = $"{projectRootPath}/{exePath}";
+                    UnityEngine.Debug.Log($"{grayBlueExe.StartInfo.FileName}");
                     //grayBlueExe.StartInfo.Arguments = "/c" + path;
                     grayBlueExe.Start();
                 } catch (Exception e) {
                     UnityEngine.Debug.Log(e.Message);
                 }
             } else if (state == PlayModeStateChange.ExitingPlayMode) {
-                grayBlueExe?.Close();
+                try {
+                    grayBlueExe?.CloseMainWindow();
+                } catch (Exception e) {
+                    UnityEngine.Debug.Log(e.Message);
+                }
             }
         }
     }
